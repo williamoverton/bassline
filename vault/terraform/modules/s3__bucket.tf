bae3ccb1a2fe662037956647f0dfbb119ef51c9e
William Overton <william@overton-it-services.com>
2018-11-07 22:19:50 +0000

    Fixed destroy

--- vault/terraform/modules/s3__bucket.tf
+++ vault/terraform/modules/s3__bucket.tf
@@ -16,9 +16,7 @@ resource "aws_s3_bucket" "bl_vault_container_config_storage" {
   bucket = "bl-vault-config-storage-${var.stack}-${var.aws_region}-${var.namespace}"
   acl    = "private"
 
-  versioning {
-    enabled = true
-  }
+  force_destroy = "true"
 
   tags {
     Name  = "bl-vault-config-storage-${var.stack}-${var.aws_region}-${var.namespace}"
