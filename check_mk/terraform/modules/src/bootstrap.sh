a7b9a1e959ab3486d453d1724c16240c68ed14ec
William Overton <william@overton-it-services.com>
2018-11-22 20:19:20 +0000

    Added check_mk docker file

---
+++ check_mk/terraform/modules/src/bootstrap.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+BASE_OMD="/opt/omd/sites"
+
+# Init check_mk instance
+if [ ! -d "${BASE_OMD}/${CMK_SITE}" ] ; then
+
+    # Create new site
+    omd create ${CMK_SITE}
+    omd config ${CMK_SITE} set APACHE_TCP_ADDR 0.0.0.0
+    htpasswd -b -m /omd/sites/${CMK_SITE}/etc/htpasswd cmkadmin ${CMK_PASSWORD}
+    /opt/redirector.sh ${CMK_SITE} > /omd/sites/${CMK_SITE}/var/www/index.html
+    
+    # Save omd record about tmpfs for second use
+    grep omd /etc/fstab > ${BASE_OMD}/${CMK_SITE}/.fstab.tmpfs
+
+else
+
+    # Create user and group for using existing site with new container
+    useradd -M -c "OMD site ${CMK_SITE}" -b "/omd/sites" -U -G omd ${CMK_SITE}
+    usermod -aG ${CMK_SITE} apache
+    chown -R ${CMK_SITE}:${CMK_SITE} ${BASE_OMD}/${CMK_SITE}
+
+    # Restore tmpfs
+    cat ${BASE_OMD}/${CMK_SITE}/.fstab.tmpfs >> /etc/fstab
+    mount tmpfs
+    omd enable ${CMK_SITE}
+fi
+
+
+# This link is always needed
+ln -s "/omd/sites/${CMK_SITE}/var/log/nagios.log" /var/log/nagios.log
+
+
+# Create SSMTP config
+CFGFILE=/etc/ssmtp/ssmtp.conf
+
+cat >$CFGFILE <<CONFIG
+root=root
+mailhub=${MAILHUB}
+FromLineOverride=YES
+CONFIG
+
+[ "${MAILHUBAUTHUSER}" ] && echo AuthUser=${MAILHUBAUTHUSER} >> $CFGFILE
+[ "${MAILHUBAUTHPASS}" ] && echo AuthPass=${MAILHUBAUTHPASS} >> $CFGFILE
+[ "${MAILHUBSTARTTLS}" ] && echo UseSTARTSSL=${MAILHUBSTARTTLS} >> $CFGFILE
+
+chmod 640 $CFGFILE
+chown root:mail $CFGFILE
+
+
+# Start cron daemon
+/usr/sbin/crond
+
+# Start check_mk
+omd start && tail -f /var/log/nagios.log
