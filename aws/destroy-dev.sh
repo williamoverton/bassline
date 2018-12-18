7ad0188344ca7cdc099b0e216faf580f21ee71cd
William Overton <william@overton-it-services.com>
2018-12-18 22:21:51 +0000

    Added check_mk to destroy

--- aws/destroy-dev.sh
+++ aws/destroy-dev.sh
@@ -2,6 +2,11 @@ echo "Destroying everything in dev!"
 
 BL_PATH=`pwd`
 
+cd check_mk/terraform/instances/dev-eu-west-1-sandbox/
+terraform destroy -auto-approve
+
+cd $BL_PATH
+
 cd vault/terraform/instances/dev-eu-west-1-sandbox/
 terraform destroy -auto-approve
 
